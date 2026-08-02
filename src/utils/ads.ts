a9ac7ba31c0dfdfa5254167fe205b62486d8c665
sagar-6435 <sagarnaidu6435@gmail.com>
2026-08-02 22:02:36 +0530

    minor

--- src/utils/ads.ts
+++ src/utils/ads.ts
@@ -1,6 +1,5 @@
-import Constants, { ExecutionEnvironment } from 'expo-constants';
 
-export const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
+export const isExpoGo = true;
 
 let BannerAd: any = () => null;
 let RewardedAd: any = null;
@@ -10,17 +9,7 @@ let BannerAdSize: any = { ANCHORED_ADAPTIVE_BANNER: 'ADAPTIVE' };
 let TestIds: any = { BANNER: 'test', INTERSTITIAL: 'test', REWARDED: 'test' };
 
 if (!isExpoGo) {
-    try {
-        const ads = require('react-native-google-mobile-ads');
-        BannerAd = ads.BannerAd;
-        RewardedAd = ads.RewardedAd;
-        useInterstitialAd = ads.useInterstitialAd;
-        RewardedAdEventType = ads.RewardedAdEventType;
-        BannerAdSize = ads.BannerAdSize;
-        TestIds = ads.TestIds;
-    } catch (e) {
-        console.error("Admob native not found");
-    }
+    // Ads removed for now
 }
 
 export { BannerAd, BannerAdSize, RewardedAd, RewardedAdEventType, TestIds, useInterstitialAd };
